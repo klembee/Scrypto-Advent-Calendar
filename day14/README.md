1d91bf18926ebdebd175d87c5031ea3f16dbf647
Clement <clement@radixdlt.com>
2022-05-04 14:05:17 -0400

    Upgraded day14

--- day14/README.md
+++ day14/README.md
@@ -9,6 +9,6 @@ Today we will me making an AlarmClock component. It will be connected to a TimeO
 5. Setup the AlarmClock component to call the "make_coffee" method on the CoffeeMachine component on 2021-12-25 07:00:00. `resim call-function [package_address] AlarmClock new [coffee_machine_component_address] make_coffee 1640433600`. Take note of the returned ResourceDef and the two component addresses. The ResourceDef is your admin badge. The first component is the TimeOracle and the second is the AlarmClock
 
 Normally, you would do a script that calls the `try_trigger` method on the AlarmClock component every x seconds. We will do it manually to keep the example simple.
-6. Call `resim call-method [alarm_clock_component_address] try_trigger 1,[admin_badge_address]`. You should see the message "Not ready yet !"
-7. Set the time to 2021-12-25 07:00:00 on the TimeOracle: `resim call-method [time_oracle] set_current_time 1640433600 1,[admin_badge_address]`
-8. Call again `resim call-method [alarm_clock_component_address] try_trigger 1,[admin_badge_address]`. You should see the message "Brewing coffee !"
+6. Call `resim run try_trigger.rtm`. You should see the message "Not ready yet !"
+7. Set the time to 2021-12-25 07:00:00 on the TimeOracle: `resim run update_current_time.rtm`
+8. Call again `resim run try_trigger.rtm`. You should see the message "Brewing coffee !"
--- day14/src/time_oracle.rs
+++ day14/src/time_oracle.rs
@@ -3,28 +3,32 @@ use scrypto::prelude::*;
 blueprint! {
     struct TimeOracle {
         // Badge used to update the time
-        admin_badge: ResourceDef,
+        admin_badge: ResourceAddress,
         second_since_unix: u64
     }
 
     impl TimeOracle {
-        pub fn new(nb_admins: u32) -> (Component, Bucket) {
+        pub fn new(nb_admins: u32) -> (ComponentAddress, Bucket) {
             // Create the admin badges
-            let admin_badges = ResourceBuilder::new_fungible(DIVISIBILITY_NONE)
+            let admin_badges = ResourceBuilder::new_fungible()
+                                .divisibility(DIVISIBILITY_NONE)
                                 .metadata("name", "UTCTimeOracle Admin Badge")
-                                .initial_supply_fungible(nb_admins);
+                                .initial_supply(nb_admins);
 
             let component = Self {
-                admin_badge: admin_badges.resource_def(),
+                admin_badge: admin_badges.resource_address(),
                 second_since_unix: 0
             }
             .instantiate();
 
+            let access_rules = AccessRules::new()
+                .method("set_current_time", rule!(require(admin_badges.resource_address())))
+                .default(rule!(allow_all));
+
             // Return the component and the admin badges
-            (component, admin_badges)
+            (component.add_access_check(access_rules).globalize(), admin_badges)
         }
 
-        #[auth(admin_badge)]
         pub fn set_current_time(&mut self, second_since_unix: u64) {
             self.second_since_unix = second_since_unix;
         }
