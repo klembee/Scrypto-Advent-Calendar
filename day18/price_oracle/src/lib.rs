288a879d25af82acc3ca07a4ee7b72c0171d01e0
Clement <clement@radixdlt.com>
2022-05-05 10:50:41 -0400

    Upgraded day18

--- day18/price_oracle/src/lib.rs
+++ day18/price_oracle/src/lib.rs
@@ -5,39 +5,39 @@ use scrypto::prelude::*;
 blueprint! {
     struct PriceOracle {
         /// Last price of each resource pair
-        prices: LazyMap<(Address, Address), Decimal>,
+        prices: LazyMap<(ResourceAddress, ResourceAddress), Decimal>,
         usd: Vault
     }
 
     impl PriceOracle {
         /// Creates a PriceOracle component, along with admin badges.
-        pub fn new() -> Component {
+        pub fn new() -> ComponentAddress {
             // Create usd tokens
-            let usd = ResourceBuilder::new_fungible(DIVISIBILITY_MAXIMUM)
+            let usd = ResourceBuilder::new_fungible()
                         .metadata("name", "USD")
-                        .initial_supply_fungible(100000);
+                        .initial_supply(100000);
 
             Self {
                 prices: LazyMap::new(),
                 usd: Vault::with_bucket(usd)
             }
-            .instantiate()
+            .instantiate().globalize()
         }
 
         /// Returns the current price of a resource pair BASE/QUOTE.
-        pub fn get_price(&self, base: Address, quote: Address) -> Option<Decimal> {
+        pub fn get_price(&self, base: ResourceAddress, quote: ResourceAddress) -> Option<Decimal> {
             self.prices.get(&(base, quote))
         }
 
         // Return the address of USD token
-        pub fn get_usd_address(&self) -> Address {
+        pub fn get_usd_address(&self) -> ResourceAddress {
             self.usd.resource_address()
         }
 
         /// Updates the price of a resource pair BASE/QUOTE and its inverse.
-        pub fn update_price(&self, base: Address, quote: Address, price: Decimal) {
+        pub fn update_price(&self, base: ResourceAddress, quote: ResourceAddress, price: Decimal) {
             self.prices.insert((base, quote), price);
-            self.prices.insert((quote, base), Decimal::from(1) / price);
+            self.prices.insert((quote, base), Decimal::one() / price);
         }
     }
 }
