9d3f1569eb77022339ea7c9d72d2e221faccb3b7
Clement <clement@radixdlt.com>
2022-05-06 15:03:56 -0400

    Migrated day23 to v0.4

--- day23/src/rating_system.rs
+++ day23/src/rating_system.rs
@@ -1,5 +1,4 @@
 use scrypto::prelude::*;
-use sbor::*;
 use crate::membership_system::*;
 
 #[derive(Decode, Encode, TypeId, Describe, Clone)]
@@ -14,51 +13,53 @@ pub struct Review {
 blueprint! {
     struct RatingSystem {
         membership_nft_admin: Vault,
-        membership_system: MembershipSystem,
-        membership_nft_def: ResourceDef,
-        reviews: HashMap<u128, Vec<Review>>
+        membership_system_address: ComponentAddress,
+        membership_nft_def: ResourceAddress,
+        reviews: HashMap<NonFungibleId, Vec<Review>>
     }
 
     impl RatingSystem {
-        pub fn new() -> Component {
-            let (membership_system_component, admin_badge): (Component, Bucket) = MembershipSystem::new();
-            let membership_system: MembershipSystem = membership_system_component.into();
+        pub fn new() -> ComponentAddress {
+            let (membership_system_address, admin_badge): (ComponentAddress, Bucket) = MembershipSystem::new();
+            let membership_system: MembershipSystem = membership_system_address.into();
             let member_nft_def = membership_system.get_member_nft_def();
 
             Self {
                 membership_nft_admin: Vault::with_bucket(admin_badge),
                 membership_nft_def: member_nft_def.into(),
-                membership_system: membership_system,
+                membership_system_address: membership_system_address,
                 reviews: HashMap::new()
             }
-            .instantiate()
+            .instantiate().globalize()
         }
 
         // As a member of the DAO
         // create a new service that you are offering
-        #[auth(membership_nft_def)]
-        pub fn create_service(&mut self, service: String){
-            let mut nft_data: MemberData = self.membership_nft_def.get_nft_data(auth.get_nft_id());
+        pub fn create_service(&mut self, service: String, auth: Proof){
+            assert_eq!(auth.resource_address(), self.membership_nft_def, "Wrong proof provided!");
+            let auth_nft = auth.non_fungible();
+
+            let mut nft_data: MemberData = auth_nft.data();
             assert!(!nft_data.services.contains(&service), "You are already offering that service.");
 
             // Update the list of services on the member NFT
             nft_data.services.push(service);
 
             // Insert empty review list for that member
-            self.reviews.insert(auth.get_nft_id(), Vec::new());
+            self.reviews.insert(auth_nft.id(), Vec::new());
 
-            self.membership_nft_admin.authorize(|badge| {
-                self.membership_nft_def.update_nft_data(auth.get_nft_id(), nft_data, badge);
+            self.membership_nft_admin.authorize(|| {
+                borrow_resource_manager!(self.membership_nft_def).update_non_fungible_data(&auth_nft.id(), nft_data);
             });
         }
 
         // As a user, review services received from a
         // member of the DAO.
-        pub fn review_service(&mut self, member_id: u128, service_name: String, offered_on: u64, stars: u8, review: String) {
+        pub fn review_service(&mut self, member_id: NonFungibleId, service_name: String, offered_on: u64, stars: u8, review: String) {
             assert!(stars <= 5, "stars param must be between 0 and 5");
-            assert!(offered_on <= Context::current_epoch(), "The service must have been offered in the past !");
+            assert!(offered_on <= Runtime::current_epoch(), "The service must have been offered in the past !");
 
-            let member_nft_data: MemberData = self.membership_nft_def.get_nft_data(member_id);
+            let member_nft_data: MemberData = borrow_resource_manager!(self.membership_nft_def).get_non_fungible_data(&member_id);
             assert!(member_nft_data.services.contains(&service_name), "The member is not offering that service");
 
             // Insert the review on the member's nft and increase their good member points
@@ -75,15 +76,15 @@ blueprint! {
                 stars: stars,
                 offered_on: offered_on,
                 text: review,
-                reviewed_on: Context::current_epoch()
+                reviewed_on: Runtime::current_epoch()
             });
 
             self.reviews.insert(member_id, review_list.clone());
         }
 
         // Display the services and ratings for a particular member
-        pub fn display_ratings(&self, member_id: u128) {
-            let member_data: MemberData = self.membership_nft_def.get_nft_data(member_id);
+        pub fn display_ratings(&self, member_id: NonFungibleId) {
+            let member_data: MemberData = borrow_resource_manager!(self.membership_nft_def).get_non_fungible_data(&member_id);
 
             info!("Service: ");
             info!("=====");
